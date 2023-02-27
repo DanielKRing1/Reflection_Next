99d4241abbdc3149c9ddcd4d6355e00a732dd183
Daniel Ring <DanielKRing1@gmail.com>
2023-02-27 15:48:15 -0800

    Organize components + pages

--- pages/history.tsx
+++ pages/history.tsx
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function History() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>History View</h1>
       <Link href="/">Go Back</Link>
     </>
