bb3dce81ed139edbc92bcc0433a65dba76b78c29
Daniel Ring <DanielKRing1@gmail.com>
2023-03-02 18:24:22 -0800

    Rename hard driver as 'DbHardware'

--- db/hardware/index.ts
+++ db/hardware/index.ts
@@ -1,2 +1,5 @@
-import DbDriver from "./localstorage";
-export default DbDriver;
+// EXPORT DESIRED DB HARDWARE FROM HERE
+// DbApi will import this db hardware and use it agnostically
+
+import DbHardware from "./localstorage";
+export default DbHardware;
