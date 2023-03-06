f5aa3c231d5d4bcc479477e75a8431a9ab8198ad
Daniel Ring <DanielKRing1@gmail.com>
2023-03-06 00:05:46 -0800

    Create ui component to display identity thoughts for reflection

--- components/pages/home/Reflecting/SelectionList/index.tsx
+++ components/pages/home/Reflecting/SelectionList/index.tsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
+import dbDriver from "../../../../../db/api";
+import { Inkling, Thought } from "../../../../../db/api/types";
 import { RootState } from "../../../../../redux/store";
+import SelectionItem from "./SelectionItem";
 
 const SelectionList = () => {
   // REDUX
-  const {} = useSelector((state: RootState) => state.newThoughtsSlice);
-  // 1. TODO Alter Journal Metadata through Redux
-  // 2. TODO Alter Journal Entries through Redux
-  // 3. TODO Cache new Journal Thoughts through Redux
+  const { activeJournalId } = useSelector(
+    (state: RootState) => state.activeJournalSlice
+  );
+  const { newInklings } = useSelector(
+    (state: RootState) => state.newInklingsSlice
+  );
+  const { thoughtsDict } = useSelector(
+    (state: RootState) => state.journalThoughtsDictSlice
+  );
+  const { identityThoughtIds } = useSelector(
+    (state: RootState) => state.identityThoughtsSlice
+  );
 
-  return <>{}</>;
+  return (
+    <>
+      {identityThoughtIds.map((id: string) => (
+        <SelectionItem
+          key={id}
+          text={thoughtsDict[id].data}
+          onClick={() => {}}
+        />
+      ))}
+
+      {newInklings.map((inkling: Inkling) => (
+        <SelectionItem
+          key={inkling.id}
+          text={inkling.data}
+          onClick={() => {}}
+        />
+      ))}
+    </>
+  );
 };
 
 export default SelectionList;
