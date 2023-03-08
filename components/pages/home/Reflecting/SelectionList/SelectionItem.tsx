6a449199ad112a6e0ad637fca37793d0edec62ab
Daniel Ring <DanielKRing1@gmail.com>
2023-03-07 23:49:43 -0800

    Highlight selected thoughts/inklings borders during reflection

--- components/pages/home/Reflecting/SelectionList/SelectionItem.tsx
+++ components/pages/home/Reflecting/SelectionList/SelectionItem.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MyButton from "../../../../generic/Button/MyButton";
 import MyText from "../../../../generic/Text/MyText";
 
 type SelectionItemProps = {
+  isSelected: boolean;
   text: string;
   onClick: () => void;
 };
 const SelectionItem = (props: SelectionItemProps) => {
-  const { text, onClick } = props;
+  const { isSelected, text, onClick } = props;
+  useEffect(() => {
+    console.log(isSelected);
+  }, [isSelected]);
 
   return (
-    <MyButton onClick={onClick}>
+    <MyButton borderColor={isSelected ? "#AAFF00" : ""} onClick={onClick}>
       <MyText>{text}</MyText>
     </MyButton>
   );
--- components/pages/home/Reflecting/SelectionList/index.tsx
+++ components/pages/home/Reflecting/SelectionList/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Inkling } from "../../../../../db/api/types";
-import { RootState } from "../../../../../redux/store";
+import {
+  addInklingId,
+  addThoughtId,
+  rmInklingId,
+  rmThoughtId,
+} from "../../../../../redux/createJournalEntrySlice";
+import { AppDispatch, RootState } from "../../../../../redux/store";
+import FlexCol from "../../../../generic/Flex/FlexCol";
 import SelectionItem from "./SelectionItem";
 
 const SelectionList = () => {
@@ -15,25 +22,43 @@ const SelectionList = () => {
   const { identityThoughtIds } = useSelector(
     (state: RootState) => state.identityThoughtsSlice
   );
+  const { selectedInklingIds, selectedThoughtIds } = useSelector(
+    (state: RootState) => state.createJournalEntrySlice
+  );
+  const dispatch: AppDispatch = useDispatch();
+
+  // HANDLERS
+  const handleToggleSelectThought = (id: string) => {
+    if (!selectedThoughtIds[id]) dispatch(addThoughtId(id));
+    else dispatch(rmThoughtId(id));
+  };
+  const handleToggleSelectInkling = (id: string) => {
+    console.log("toggle");
+    console.log(id);
+    if (!selectedInklingIds[id]) dispatch(addInklingId(id));
+    else dispatch(rmInklingId(id));
+  };
 
   return (
-    <>
+    <FlexCol>
       {identityThoughtIds.map((id: string) => (
         <SelectionItem
           key={id}
+          isSelected={selectedThoughtIds[id] || false}
           text={thoughtsDict[id].data}
-          onClick={() => {}}
+          onClick={() => handleToggleSelectThought(id)}
         />
       ))}
 
       {newInklings.map((inkling: Inkling) => (
         <SelectionItem
           key={inkling.id}
+          isSelected={selectedInklingIds[inkling.id] || false}
           text={inkling.data}
-          onClick={() => {}}
+          onClick={() => handleToggleSelectInkling(inkling.id)}
         />
       ))}
-    </>
+    </FlexCol>
   );
 };
 
