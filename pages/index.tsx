052d845d0aeaea71e7b67b20048ac7cdcb9bfd17
Daniel Ring <DanielKRing1@gmail.com>
2023-03-08 17:12:10 -0800

    Add border radius to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,6 +34,7 @@ export default function Home() {
               width: "70vw",
               borderWidth: "5rem",
               border: "solid black",
+              borderRadius: "0.4rem",
               boxShadow:
                 "inset 0 -3em 3em rgb(0 0 0 / 10%), 0.5em 0.5em 2em rgb(0 0 0 / 30%)",
             }}
