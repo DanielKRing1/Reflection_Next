52974f5a8ca5902c262fe4edfb93a365b63de5fd
Daniel Ring <DanielKRing1@gmail.com>
2023-03-21 15:24:19 -0700

    Create apollo gql provider

---
+++ graphql/apollo/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+// APOLLO CLIENT
+
+const client = new ApolloClient({
+  // TODO Remove hardcode after testing
+  uri: "localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
+// APOLLO PROVIDER
+
+type ApolloProviderProps = { children: React.ReactNode };
+export default (props: ApolloProviderProps) => {
+  const { children } = props;
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { Provider as ReduxProvider } from "react-redux";
 
 // MY PROVIDERS
 import MyThemeProvider from "../theme/ThemeProvider";
+import ApolloProvider from "../graphql/apollo";
 
 // REDUX
 import store from "../redux/store";
@@ -11,11 +12,13 @@ import OnStartUpWrapper from "../components/startUp";
 export default function MyApp({ Component, pageProps }) {
   return (
     <ReduxProvider store={store}>
-      <MyThemeProvider>
-        <OnStartUpWrapper>
-          <Component {...pageProps} />
-        </OnStartUpWrapper>
-      </MyThemeProvider>
+      <ApolloProvider>
+        <MyThemeProvider>
+          <OnStartUpWrapper>
+            <Component {...pageProps} />
+          </OnStartUpWrapper>
+        </MyThemeProvider>
+      </ApolloProvider>
     </ReduxProvider>
   );
 }
