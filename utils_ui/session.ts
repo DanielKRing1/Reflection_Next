79ce4f4ae2dea9aef2a0bb74df15ee1f9fe5f6bc
Daniel Ring <DanielKRing1@gmail.com>
2023-04-24 00:03:49 -0700

    Implement 'session' utils_ui methods to check which cookies are present

--- utils_ui/session.ts
+++ utils_ui/session.ts
@@ -1,3 +1,9 @@
+import { parseJSONCookie } from "../utils/cookie";
+import { isDateOlder } from "../utils/date";
+
+const META_ACCESS_SESSION_COOKIE_NAME = "rfltn-access-meta";
+const META_REFRESH_SESSION_COOKIE_NAME = "rfltn-refresh-meta";
+
 // LOGIN
 
 export enum NeededLoginAction {
@@ -6,11 +12,37 @@ export enum NeededLoginAction {
     None,
 }
 
+/**
+ *
+ * @returns Whether access session cookie has expired, based on access session meta cookie's 'expires' attribute
+ * If no cookie present or no 'expires' attribute, returns false
+ */
 export const hasFreshAccessCookie = (): boolean => {
-    return true;
+    try {
+        const { expires } = parseJSONCookie(META_ACCESS_SESSION_COOKIE_NAME);
+
+        // Fresh (return true) if now is 'older' than expiration
+        return isDateOlder(new Date(), expires as Date);
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
 };
+/**
+ *
+ * @returns Whether refresh session cookie has expired, based on refresh session meta cookie's 'expires' attribute
+ * If no cookie present or no 'expires' attribute, returns false
+ */
 export const hasFreshRefreshCookie = (): boolean => {
-    return true;
+    try {
+        const { expires } = parseJSONCookie(META_REFRESH_SESSION_COOKIE_NAME);
+
+        // Fresh (return true) if now is 'older' than expiration
+        return isDateOlder(new Date(), expires as Date);
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
 };
 export const getNeededLoginAction = (): NeededLoginAction => {
     if (hasFreshAccessCookie()) return NeededLoginAction.None;
