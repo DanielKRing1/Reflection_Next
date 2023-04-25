ea9260761acd87329981341e40da05e5e46fbcb7
Daniel Ring <DanielKRing1@gmail.com>
2023-04-24 22:05:39 -0700

    Defined type policy (id generation) for apollo Inklings, JournalEntry, Thoughts)

--- graphql/apollo/client.ts
+++ graphql/apollo/client.ts
@@ -1,12 +1,28 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+// TYPW POLICIES
+
+const typePolicies = {
+    Inkling: {
+        keyFields: ["upc"],
+    },
+    JournalEntry: {
+        keyFields: ["upc"],
+    },
+    Thought: {
+        keyFields: ["upc"],
+    },
+};
+
 // APOLLO CLIENT
 
 const client = new ApolloClient({
     // TODO Remove hardcode after testing
     uri: "localhost:4000/graphql",
     credentials: "include",
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+        typePolicies,
+    }),
 });
 
 export default client;
