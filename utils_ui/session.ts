32c73c009166890001862db4858c48f693fcdc61
Daniel Ring <DanielKRing1@gmail.com>
2023-04-27 15:00:07 -0700

    Create additional session utils to check if access or refresh cookies are 'getting stale'

--- utils_ui/session.ts
+++ utils_ui/session.ts
@@ -8,9 +8,36 @@ const META_REFRESH_SESSION_COOKIE_NAME = "rfltn-refresh-meta";
 
 export enum NeededSessionAction {
     Login,
+    ReLogin,
     Refresh,
     None,
 }
+
+// ACCESS
+
+/**
+ *
+ * @param staleTtl The remaining lifespan of the access cookie in ms, that would make it considered 'getting stale'
+ * default is 30 min (1000*60*30)
+ * @returns
+ */
+export const isAccessCookieGettingStale = (
+    staleTtl: number = 1000 * 60 * 30
+): boolean => {
+    try {
+        const { expires } = parseJSONCookie(META_ACCESS_SESSION_COOKIE_NAME);
+        if (expires === undefined)
+            throw new Error(
+                "isAccessCookieGettingStale did not find an access meta cookie"
+            );
+
+        // Stale (return false) if ttl from now is <= staleTtl
+        return (expires as Date).getTime() - Date.now() <= staleTtl;
+    } catch (err) {
+        console.log(err);
+        return true;
+    }
+};
 
 /**
  *
@@ -22,7 +49,7 @@ export const hasFreshAccessCookie = (): boolean => {
         const { expires } = parseJSONCookie(META_ACCESS_SESSION_COOKIE_NAME);
         if (expires === undefined)
             throw new Error(
-                "hasFreshAccessCookie did not find a access meta cookie"
+                "hasFreshAccessCookie did not find an access meta cookie"
             );
 
         // Fresh (return true) if now is 'older' than expiration
@@ -32,7 +59,36 @@ export const hasFreshAccessCookie = (): boolean => {
         return false;
     }
 };
+
+// REFRESH
+
 /**
+ *
+ * @param staleTtl The remaining lifespan of the access cookie in ms, that would make it considered 'getting stale'
+ * default is 10 seconds (1000*10)
+ * My idea is that if the refresh cookie will expire within the next 10 seconds, there 'is not enough time' (maybe) to renew the refresh token
+ * @returns
+ */
+export const isRefreshCookieGettingStale = (
+    staleTtl: number = 1000 * 10
+): boolean => {
+    try {
+        const { expires } = parseJSONCookie(META_REFRESH_SESSION_COOKIE_NAME);
+        if (expires === undefined)
+            throw new Error(
+                "isRefreshCookieGettingStale did not find a refresh meta cookie"
+            );
+
+        // Stale (return false) if ttl from now is <= staleTtl
+        return (expires as Date).getTime() - Date.now() <= staleTtl;
+    } catch (err) {
+        console.log(err);
+        return true;
+    }
+};
+
+/**
+ * Server already regenerates refresh cookie when > half expired, so no refresh logic needed
  *
  * @returns Whether refresh session cookie has expired, based on refresh session meta cookie's 'expires' attribute
  * If no cookie present or no 'expires' attribute, returns false
