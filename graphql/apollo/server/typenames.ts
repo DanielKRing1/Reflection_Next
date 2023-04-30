09e98a4c72cc6111a6847a62f18b8e64c193b065
Daniel Ring <DanielKRing1@gmail.com>
2023-04-29 17:15:40 -0700

    Create constants file to help with cache id generation

---
+++ graphql/apollo/server/typenames.ts
@@ -0,0 +1,8 @@
+export const USER_TYPENAME = "User";
+export const JOURNAL_TYPENAME = "Journal";
+
+export const JOURNAL_ENTRY_TYPENAME = "JouralEntry";
+export const THOUGHT_TYPENAME = "Thought";
+export const REFLECTION_TYPENAME = "Reflection";
+
+export const INKLING_TYPENAME = "Inkling";
