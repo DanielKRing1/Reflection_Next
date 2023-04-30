0e0c58d3927846767d7f1d487314e9ca8d5779a1
Daniel Ring <DanielKRing1@gmail.com>
2023-04-29 17:10:35 -0700

    Create filed policy to add 'thought' field to reflections in apollo client

---
+++ graphql/apollo/client/fieldPolicy/reflection.ts
@@ -0,0 +1,26 @@
+import { FieldFunctionOptions, gql } from "@apollo/client";
+
+import { genCacheId } from "../../local/utils/id";
+import { THOUGHT_TYPENAME } from "../../server/typenames";
+
+import { Thought } from "../../../../db/api/types";
+
+export function read(reflections, { cache }: FieldFunctionOptions) {
+    return reflections.map((r) => {
+        const { thoughtId } = r;
+        const thoughtCacheId: string = genCacheId(THOUGHT_TYPENAME, thoughtId);
+
+        const thought: Thought = cache.readFragment({
+            id: thoughtCacheId,
+            fragment: gql`
+                fragment MyThought on Thought {
+                    timeId
+                    journalId
+                    text
+                }
+            `,
+        });
+
+        return { ...r, thought };
+    });
+}
