1e8b9145446ec55f39d8f219ef876c0ebfad40b6
Daniel Ring <DanielKRing1@gmail.com>
2023-05-01 23:18:22 -0700

    Formatting

--- theme/ThemeProvider.tsx
+++ theme/ThemeProvider.tsx
@@ -8,21 +8,23 @@ import { ThemeMode, lightTheme, darkTheme, GlobalStyles } from "./themeConfig";
 import ThemeToggleProvider, { ThemeToggleContext } from "./ThemeToggle";
 
 type MyThemeProviderProps = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 const MyThemeProvider = ({ children }: MyThemeProviderProps) => {
-  const { mode } = useContext(ThemeToggleContext);
+    const { mode } = useContext(ThemeToggleContext);
 
-  return (
-    <ThemeToggleProvider>
-      <ThemeProvider theme={mode === ThemeMode.light ? lightTheme : darkTheme}>
-        {/* Inject Globl styles */}
-        <GlobalStyles />
-        {/* Display current Page */}
-        {children}
-      </ThemeProvider>
-    </ThemeToggleProvider>
-  );
+    return (
+        <ThemeToggleProvider>
+            <ThemeProvider
+                theme={mode === ThemeMode.light ? lightTheme : darkTheme}
+            >
+                {/* Inject Global styles */}
+                <GlobalStyles />
+                {/* Display current Page */}
+                {children}
+            </ThemeProvider>
+        </ThemeToggleProvider>
+    );
 };
 
 export default MyThemeProvider;
