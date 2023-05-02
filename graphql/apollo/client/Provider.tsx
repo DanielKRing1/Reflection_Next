863068422dcdb1220c88a2cfffab1410ef390e17
Daniel Ring <DanielKRing1@gmail.com>
2023-05-02 00:53:45 -0700

    Move top-level hooks to apollo provider component     Since they require apollo client state

--- graphql/apollo/client/Provider.tsx
+++ graphql/apollo/client/Provider.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ApolloProvider } from "@apollo/client";
 
 import client from "./client";
+import AppPhaseManager from "./AppPhaseManager";
 
 // APOLLO PROVIDER
 
@@ -9,5 +10,9 @@ type ApolloProviderProps = { children: React.ReactNode };
 export default (props: ApolloProviderProps) => {
     const { children } = props;
 
-    return <ApolloProvider client={client}>{children}</ApolloProvider>;
+    return (
+        <ApolloProvider client={client}>
+            <AppPhaseManager>{children}</AppPhaseManager>
+        </ApolloProvider>
+    );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,19 +8,9 @@ import ApolloProvider from "../graphql/apollo/client/Provider";
 // REDUX
 import store from "../redux/store";
 
-// TOP-LEVEL HOOKS
-import useLoginChange from "../appHooks/useLoginChange";
-import useHydrateApp from "../appHooks/useHydrateApp";
-import useHydrateJournal from "../appHooks/useHydrateJournal";
-import useJournalPhase from "../appHooks/useJournalPhase";
-
 export default function MyApp({ Component, pageProps }) {
     // Will redirect to '/login' page on logout
     // and '/' on login
-    useLoginChange();
-    useHydrateApp();
-    useHydrateJournal();
-    useJournalPhase();
 
     return (
         <ReduxProvider store={store}>
