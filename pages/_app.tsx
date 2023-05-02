d971a5370ff65b0bbb758f7de3133497d4e1883f
Daniel Ring <DanielKRing1@gmail.com>
2023-05-01 17:03:46 -0700

    Use app-level hooks in _app.tsx container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,30 @@ import { Provider as ReduxProvider } from "react-redux";
 
 // MY PROVIDERS
 import MyThemeProvider from "../theme/ThemeProvider";
-import ApolloProvider from "../graphql/apollo/Provider";
+import ApolloProvider from "../graphql/apollo/client/Provider";
 
 // REDUX
 import store from "../redux/store";
-import OnStartUpWrapper from "../components/startUp";
+
+// TOP-LEVEL HOOKS
+import useLoginChange from "../appHooks/useLoginChange";
+import useHydrateApp from "../appHooks/useHydrateApp";
+import useHydrateJournal from "../appHooks/useHydrateJournal";
+import useJournalPhase from "../appHooks/useJournalPhase";
 
 export default function MyApp({ Component, pageProps }) {
+    // Will redirect to '/login' page on logout
+    // and '/' on login
+    useLoginChange();
+    useHydrateApp();
+    useHydrateJournal();
+    useJournalPhase();
+
     return (
         <ReduxProvider store={store}>
             <ApolloProvider>
                 <MyThemeProvider>
-                    <OnStartUpWrapper>
-                        <Component {...pageProps} />
-                    </OnStartUpWrapper>
+                    <Component {...pageProps} />
                 </MyThemeProvider>
             </ApolloProvider>
         </ReduxProvider>
