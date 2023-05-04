85f07bfaa6ad179c3f4465cf0b62ca20f8af2288
Daniel Ring <DanielKRing1@gmail.com>
2023-05-03 17:19:45 -0700

    Add option to clear local active journal

--- graphql/apollo/local/state/activeJournal.ts
+++ graphql/apollo/local/state/activeJournal.ts
@@ -6,4 +6,6 @@ const activeJournalVar = makeVar<string>(null);
 export const setActiveJournal = (journalId: string) =>
     activeJournalVar(journalId);
 
+export const clearActiveJournal = () => activeJournalVar(null);
+
 export const getActiveJournal = () => activeJournalVar();
