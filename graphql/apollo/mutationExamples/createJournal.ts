095a22ea08f50b41ba42bacda5531c272b167022
Daniel Ring <DanielKRing1@gmail.com>
2023-05-04 20:28:33 -0700

    Edit create journal update  method to add new journal to local cache

--- graphql/apollo/mutationExamples/createJournal.ts
+++ graphql/apollo/mutationExamples/createJournal.ts
@@ -5,36 +5,45 @@ import {
     setActiveJournal,
 } from "../local/state/activeJournal";
 import { setJournalPhaseInklings } from "../local/state/journalPhase";
-import { CREATE_JOURNAL } from "../../gql/journal";
+import { CREATE_JOURNAL, GET_JOURNALS } from "../../gql/journal";
+import { Journal } from "../../../db/api/types";
 
 export default (): MutationTuple<any, any, any, any> => {
-    const activeJournalId: string = getActiveJournal();
-
     /**
      * 1. Create journal on server
      * 2. Add journal locally
      * 3. When journal list changes, select first journal if no active journal
      */
 
-    const [commitInklings, handle] = useMutation(CREATE_JOURNAL, {
-        variables: {
-            journalId: activeJournalId,
-        },
-        update(
-            cache,
-            {
+    const [createJournal, handle] = useMutation(CREATE_JOURNAL, {
+        update(cache, { data: { createJournal } }) {
+            console.log("Create Journal update method:");
+            console.log(createJournal);
+
+            const { id, userId, name } = createJournal;
+            console.log(id);
+
+            // 1. Add created journal to local cache
+            const { journals: existingJournals }: { journals: Journal[] } =
+                cache.readQuery({
+                    query: GET_JOURNALS,
+                });
+
+            cache.writeQuery({
+                query: GET_JOURNALS,
                 data: {
-                    CreateJournal: { id, userId, name },
+                    journals: [...existingJournals, { id, userId, name }],
                 },
-            }
-        ) {
-            // 1. If no active journal id, set to new journal id
+            });
+
+            // TODO: Check if this works -> Did I destructure the response data properly?
+            // 2. If no active journal id, set to new journal id
             if (getActiveJournal() === null) setActiveJournal(id);
 
-            // 2. Set JournalPhase to 'Reflection'
+            // 3. Set JournalPhase to 'Reflection'
             setJournalPhaseInklings();
         },
     });
 
-    return [commitInklings, handle];
+    return [createJournal, handle];
 };
