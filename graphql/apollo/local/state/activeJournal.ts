3b3b723556f336359e66393267083a138a6fbf8a
Daniel Ring <DanielKRing1@gmail.com>
2023-05-08 01:52:58 -0700

    Export active journal reactive var

--- graphql/apollo/local/state/activeJournal.ts
+++ graphql/apollo/local/state/activeJournal.ts
@@ -1,11 +1,12 @@
 import { makeVar } from "@apollo/client";
 
 // Initializes to null
-const activeJournalVar = makeVar<string>(null);
+export const activeJournalVar = makeVar<string>(null);
 
 export const setActiveJournal = (journalId: string) =>
     activeJournalVar(journalId);
 
 export const clearActiveJournal = () => activeJournalVar(null);
 
+export const getActiveJournalVar = () => activeJournalVar;
 export const getActiveJournal = () => activeJournalVar();
