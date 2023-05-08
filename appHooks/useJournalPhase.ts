cd8c428c469d9ee82384165640f10f5d5a3271dd
Daniel Ring <DanielKRing1@gmail.com>
2023-05-08 01:50:38 -0700

    Fix journal phase app-level hook logic

--- appHooks/useJournalPhase.ts
+++ appHooks/useJournalPhase.ts
@@ -2,23 +2,25 @@
  * Only use this hook once at the top-level of the app
  */
 
-import { useLazyQuery, useQuery } from "@apollo/client";
+import { useQuery, useReactiveVar } from "@apollo/client";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-import { GET_JOURNAL_PHASE } from "../graphql/apollo/local/gql/journalPhase";
 import { JournalPhase } from "../utils_ui/journalPhase";
-import { determineJournalPhase } from "../graphql/apollo/local/state/journalPhase";
+import {
+    determineJournalPhase,
+    journalPhaseVar,
+} from "../graphql/apollo/local/state/journalPhase";
 import { GET_INKLINGS } from "../graphql/gql/inklings";
-import { GET_ACTIVE_JOURNAL } from "../graphql/apollo/local/gql/activeJournal";
+import { activeJournalVar } from "../graphql/apollo/local/state/activeJournal";
+import { GET_JOURNALS } from "../graphql/gql/journal";
+import { GET_USER } from "../graphql/gql/user";
 
 export default () => {
     // LOCAL STATE
 
-    const { data: { activeJournal = null } = {} } =
-        useQuery(GET_ACTIVE_JOURNAL);
-    const { data: { journalPhase = JournalPhase.Unknown } = {} } =
-        useQuery(GET_JOURNAL_PHASE);
+    const activeJournal = useReactiveVar(activeJournalVar);
+    const journalPhase = useReactiveVar(journalPhaseVar);
 
     // ROUTING
 
@@ -26,33 +28,43 @@ export default () => {
 
     // SERVER/LOCAL STATE
 
-    // Inklings
-    const [getInklings, { loading, error, data, refetch, called }] =
-        useLazyQuery(GET_INKLINGS);
+    // Local User, Journal, Inklings
+    const {
+        loading: loading_user,
+        error: error_user,
+        data: { user = null } = {},
+        refetch: refetch_user,
+    } = useQuery(GET_USER, {
+        fetchPolicy: "cache-only",
+    });
+    const {
+        loading: loading_journals,
+        error: error_journals,
+        data: { journals = null } = {},
+        refetch: refetch_journals,
+    } = useQuery(GET_JOURNALS, {
+        fetchPolicy: "cache-only",
+    });
+    const {
+        loading: loading_inklings,
+        error: error_inklings,
+        data: { inklings = null } = {},
+        refetch: refetch_inklings,
+    } = useQuery(GET_INKLINGS, {
+        variables: {
+            journalId: activeJournal,
+        },
+        fetchPolicy: "cache-only",
+    });
 
     // SET JOURNAL PHASE
 
-    // Cache Inklings from server, locally
-    const _getInklings = () => {
-        const vars = {
-            variables: {
-                journalId: activeJournal,
-            },
-        };
-
-        if (!called) getInklings(vars);
-        else refetch(vars);
-    };
+    // After Inklings are cached, determine journal phases
     useEffect(() => {
-        if (activeJournal === null) return;
+        if (user === null || journals === null || inklings === null) return;
 
-        _getInklings();
-    }, [activeJournal]);
-
-    // When Inklings are cached, determine journal phase
-    useEffect(() => {
-        if (!loading && !error) determineJournalPhase();
-    }, [loading, error]);
+        determineJournalPhase();
+    }, [user, journals, inklings]);
 
     // HANDLE JOURNAL PHASE CHANGES
 
--- appHooks/useSyncActiveJournal.ts
+++ appHooks/useSyncActiveJournal.ts
@@ -2,28 +2,39 @@
  * Only use this hook once at the top-level of the app
  */
 
-import { useMutation, useQuery } from "@apollo/client";
+import { useMutation, useQuery, useReactiveVar } from "@apollo/client";
 import { useEffect } from "react";
 
 import { GET_USER } from "../graphql/gql/user";
-import { setActiveJournal } from "../graphql/apollo/local/state/activeJournal";
-import { GET_ACTIVE_JOURNAL } from "../graphql/apollo/local/gql/activeJournal";
+import {
+    activeJournalVar,
+    setActiveJournal,
+} from "../graphql/apollo/local/state/activeJournal";
 import { UPDATE_LAST_USEDJID } from "../graphql/gql/updateUser";
+import { GET_JOURNALS } from "../graphql/gql/journal";
 
 export default () => {
     // LOCAL STATE
 
-    const { data: { activeJournal = null } = {} } =
-        useQuery(GET_ACTIVE_JOURNAL);
+    const activeJournal = useReactiveVar(activeJournalVar);
 
     // SERVER STATE
 
     // Query User (only from cache)
     const {
-        loading: loading_user,
-        error: error_user,
-        data: { user = { lastUsedJId: null } } = {},
+        // loading: loading_user,
+        // error: error_user,
+        data: { user = null } = {},
     } = useQuery(GET_USER, {
+        fetchPolicy: "cache-only",
+    });
+
+    // Query Journal (only from cache)
+    const {
+        // loading: loading_journals,
+        // error: error_journals,
+        data: { journals = null } = {},
+    } = useQuery(GET_JOURNALS, {
         fetchPolicy: "cache-only",
     });
 
@@ -35,22 +46,31 @@ export default () => {
 
     // SET USER'S LAST ACTIVE JOURNAL
 
+    // Determine initial starting active journal, from user.lastUsedJId and journals list
     useEffect(() => {
-        try {
-            console.log("useSyncActiveJournal-update");
-            console.log(user);
+        // 0.1. If active journal already set, do not set again
+        if (activeJournal !== null) return;
+        // 0.2. If user or journals queries have not completed yet, wait
+        if (user === null || journals === null) return;
 
-            if (user.lastUsedJId === activeJournal) return;
+        // 1. Determine the initial active journal (before any active journals have been set yet)
+        const initialActiveJournal =
+            user.lastUsedJId !== null
+                ? // Use last used journal if not null
+                  user.lastUsedJId
+                : journals.length > 0
+                ? // Else first journal in list if any exist
+                  journals[0].id
+                : // Else still null
+                  null;
 
-            setActiveJournal(user.lastUsedJId);
-        } catch (err) {
-            console.log(err);
-        }
-    }, [user]);
+        // 2. Set initial active journal, if not null
+        if (initialActiveJournal !== null)
+            setActiveJournal(initialActiveJournal);
+    }, [user, journals]);
 
     useEffect(() => {
         if (activeJournal === null) return;
-
         updateLastUsedJId({ variables: { journalId: activeJournal } });
     }, [activeJournal]);
 };
