24611396186cce305b52b2d4670062c0a3fb47c6
Daniel Ring <DanielKRing1@gmail.com>
2023-05-08 01:44:12 -0700

    Update useQueryComplete to not update if data has not changed

--- hooks/useQueryComplete.ts
+++ hooks/useQueryComplete.ts
@@ -1,5 +1,5 @@
 import { ApolloError } from "@apollo/client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function <T>(
     loading: boolean,
@@ -8,8 +8,12 @@ export default function <T>(
     onComplete: (data: T) => void,
     onError: (data: T, error: any) => void = () => {}
 ) {
+    const [_data, _setData] = useState(null);
+
     useEffect(() => {
-        if (!loading && !error) onComplete(data);
-        else if (!loading && error) onError(data, error);
+        if (!loading && !error && _data !== data) {
+            onComplete(data);
+            _setData(data);
+        } else if (!loading && error) onError(data, error);
     }, [loading, error, data, onComplete, onError]);
 }
