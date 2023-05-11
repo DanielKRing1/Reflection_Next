c476b28a7df55c14b798864a0962032d6b9f4536
Daniel Ring <DanielKRing1@gmail.com>
2023-05-10 20:12:34 -0700

    Center circle button content

--- components/generic/Button/Base/CircleButton.tsx
+++ components/generic/Button/Base/CircleButton.tsx
@@ -3,15 +3,19 @@ import styled from "styled-components";
 import MyButton from "./MyButton";
 
 type CircleButtonProps = {
-  radius: number;
+    radius: number;
 };
 const CircleButton = styled(MyButton)<CircleButtonProps>`
-  height: ${({ radius }) => `${radius}vw`};
-  width: ${({ radius }) => `${radius}vw`};
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-  border-radius: 50%;
-  padding: 1rem 1rem;
-  margin: 1rem;
+    height: ${({ radius }) => `${radius}vw`};
+    width: ${({ radius }) => `${radius}vw`};
+
+    border-radius: 50%;
+    padding: 1rem 1rem;
+    margin: 1rem;
 `;
 
 export default CircleButton;
