7e946322cd30cf278ec289627a70271bafa60c58
Daniel Ring <DanielKRing1@gmail.com>
2023-05-17 01:24:57 -0700

    Edit box shadow component

--- components/generic/BoxShadow/index.tsx
+++ components/generic/BoxShadow/index.tsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
 export default styled.div`
-    border-width: 5rem;
-    border: solid black;
-    border-radius: 0.4rem;
+    display: flex;
+    width: 100%;
     box-shadow: inset 0 -3em 3em rgb(0 0 0 / 10%),
         0.5em 0.5em 2em rgb(0 0 0 / 30%);
 `;
