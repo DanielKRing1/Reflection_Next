63a5edf6d12508c9f329b715877abe72854d7d68
Daniel Ring <DanielKRing1@gmail.com>
2023-05-17 16:42:19 -0700

    Update use protected router hook to mirror nextjs router.push args + return type

--- hooks/useProtectedRouter.ts
+++ hooks/useProtectedRouter.ts
@@ -1,9 +1,16 @@
 import { useReactiveVar } from "@apollo/client";
 import { useRouter } from "next/router";
+import { Url } from "next/dist/shared/lib/router/router";
 
 import { journalPhaseVar } from "../graphql/apollo/local/state/journalPhase";
 import { JournalPhase } from "../utils_ui/journalPhase";
 import { CREATE_JOURNAL_PATH } from "../routing/paths";
+
+interface TransitionOptions {
+    shallow?: boolean; // 浅いルーティングにするか (デフォは false)
+    locale?: string | false; // 新しいページのロケール設定
+    scroll?: boolean; // 遷移後に先頭にスクロールするか (デフォはtrue)
+}
 
 export default () => {
     // ROUTER
@@ -13,14 +20,18 @@ export default () => {
     const journalPhase = useReactiveVar(journalPhaseVar);
 
     // PROTECTED ROUTER METHODS
-    const protectedPush = (path: string) => {
+    const protectedPush = (
+        url: Url,
+        as?: Url,
+        options?: TransitionOptions
+    ): Promise<boolean> => {
         if (
             journalPhase === JournalPhase.CreateJournal &&
             router.pathname === CREATE_JOURNAL_PATH
         )
             return;
 
-        router.push(path);
+        return router.push(url, as, options);
     };
 
     return {
