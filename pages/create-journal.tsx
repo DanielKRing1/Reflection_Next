6e8e05266fa0ac7e9a13c38e1b837d4e920fabaa
Daniel Ring <DanielKRing1@gmail.com>
2023-05-17 16:35:51 -0700

    Create create-journal page

---
+++ pages/create-journal.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Link from "next/link";
+
+import styles from "../styles/Home.module.css";
+import CreateJournal from "../components/pages/home/CreateJournal";
+
+export default function History() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Create Journal</title>
+            </Head>
+
+            <main>
+                <CreateJournal />
+            </main>
+        </div>
+    );
+}
