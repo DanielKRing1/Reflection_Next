ab9476c7a6c95c60a5dd29e3e8782d9f5b5d1062
Daniel Ring <DanielKRing1@gmail.com>
2023-05-18 13:05:52 -0700

    Fix login route path constant

--- routing/paths.ts
+++ routing/paths.ts
@@ -1,4 +1,4 @@
 export const HOME_PATH = "/";
-export const LOGIN_PATH = "/login-journal";
+export const LOGIN_PATH = "/login";
 export const HISTORY_PATH = "/history";
 export const CREATE_JOURNAL_PATH = "/create-journal";
