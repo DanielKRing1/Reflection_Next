f6835d4f3c21729ead58f8a5fd9c6ecfc3cc0e5a
Daniel Ring <DanielKRing1@gmail.com>
2023-06-29 14:27:18 -0700

    Logout by erasing cookies rather than just editing local state

--- components/generic/Nav/NavBar/Logout.tsx
+++ components/generic/Nav/NavBar/Logout.tsx
@@ -3,14 +3,14 @@ import styled from "styled-components";
 
 import { MyTextNoMargin } from "../../Text/MyText";
 import ClickContainer from "../../Button/Base/ClickContainer";
-import { logoutLocal } from "../../../../graphql/apollo/local/state/isLoggedIn";
+import { logout } from "../../../../session/logout";
 
 type Props = {};
 
 export default (props: Props) => {
     // HANDLERS
     const handleLogout = () => {
-        logoutLocal();
+        logout();
     };
 
     return (
